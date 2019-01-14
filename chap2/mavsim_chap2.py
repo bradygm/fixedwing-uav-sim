0554dec95e8c9c23d7dceb1817bfae09d7c7c3f2
Brady Moon <bradygmoon@gmail.com>
2019-01-14 10:16:53 -0700

    Updated chapter 2 demonstration

--- chap2/mavsim_chap2.py
+++ chap2/mavsim_chap2.py
@@ -26,12 +26,22 @@ sim_time = SIM.start_time
 # main simulation loop
 while sim_time < SIM.end_time:
     #-------vary states to check viewer-------------
-    state.pn = sim_time # 10*sim_time
-    state.pe = 0 # 10*sim_time
-    state.h = 0 # 10*sim_time
-    state.phi = 0 # sim_time #roll
-    state.theta = sim_time #pitch
-    state.psi = 0 #yaw
+
+    if sim_time < SIM.end_time/6:
+        state.pn += 10*SIM.ts_simulation # 10*sim_time
+    elif sim_time < 2*SIM.end_time/6:
+        state.pe += 10*SIM.ts_simulation # 10*sim_time
+    elif sim_time < 3*SIM.end_time/6:
+        state.h += 10*SIM.ts_simulation # 10*sim_time
+    elif sim_time < 4*SIM.end_time/6:
+        state.phi += .2*SIM.ts_simulation # sim_time #roll
+    elif sim_time < 5*SIM.end_time/6:
+        state.theta += .2*SIM.ts_simulation #pitch
+    else:
+        state.psi += .2*SIM.ts_simulation #yaw
+
+
+
 
     #-------update viewer-------------
     mav_view.update(state)
--- parameters/simulation_parameters.py
+++ parameters/simulation_parameters.py
@@ -7,10 +7,8 @@ import numpy as np
 ######################################################################################
 ts_simulation = 0.02  # smallest time step for simulation
 start_time = 0.  # start time for simulation
-end_time = 1000.  # end time for simulation
+end_time = 100.  # end time for simulation
 
 ts_plotting = 0.5  # refresh rate for plots
 
 ts_control = ts_simulation  # sample rate for the controller
-
-
